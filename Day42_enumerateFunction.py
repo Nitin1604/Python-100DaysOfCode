44abd653f31de9915269a281544650e9c846af8d
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:42:37 +0530

    Enumerate Functions

---
+++ Day42_enumerateFunction.py
@@ -0,0 +1,15 @@
+# Day 42
+
+marks = [12, 56, 32, 98, 12,  45, 1, 4]
+
+index = 0
+for mark in marks:
+  print(mark)
+  if(index == 3):
+    print("Nitin, awesome!")
+  index +=1
+
+for index, mark in enumerate(marks, start=1):
+  print(mark)
+  if(index == 7):
+    print("Nitin, awesome!")
