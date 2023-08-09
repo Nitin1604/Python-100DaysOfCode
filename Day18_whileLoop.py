77d7c72791589286d863a6798053318b8a922fbb
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:28:18 +0530

    While Loop

---
+++ Day18_whileLoop.py
@@ -0,0 +1,16 @@
+# Day 18
+i = int(input("Enter the number: "))
+print(i)
+while(i<=5):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("While Loop Terminates")
+
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("It goes inside else block")
+
