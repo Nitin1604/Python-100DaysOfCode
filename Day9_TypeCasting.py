0e594a646376a2e6c70fbe07ccb02c79fd0542ac
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:23:56 +0530

    Type Casting

---
+++ Day9_TypeCasting.py
@@ -0,0 +1,13 @@
+# Day 9
+a = '16'
+b = '25'
+
+print(int(a)+int(b))  # String a is converted to int now... This is type casting
+
+# Implicit TypeCasting
+c = 8.9
+d = 8
+
+print(c+d)
+print(str(c) + str(d))
+
