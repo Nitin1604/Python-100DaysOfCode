39f7839dea409f354bedc1a435ef2ded97e01eef
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:21:16 +0530

    Modules and Pip

---
+++ Day3_module&pip.py
@@ -0,0 +1,11 @@
+# Day 3
+
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+pandas.read_csv("zoo.csv")
+m = hashlib.sha256()
+
