9c61dbc196bf98684d86c02e906d7e7542e26f18
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 20:44:33 +0530

    Difference between 'is' and '==' in Python

---
+++ Day54_is vs ==.py
@@ -0,0 +1,8 @@
+# Day 54
+
+a = None
+b = None
+
+print('a is b:',a is b) # exact location of object in memory
+print('a is None:',a is None) # exact location of object in memory
+print('a == b:',a == b) # value
