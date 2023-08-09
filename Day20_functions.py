0d1cc526648a23e65143e08743023f8df173544a
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:29:05 +0530

    Functions

---
+++ Day20_functions.py
@@ -0,0 +1,27 @@
+# Day 20
+def calculateGmean(a, b):
+  mean = (a*b)/(a+b)
+  print(mean)
+
+def isGreater(a, b):
+  if(a>b):
+    print("First number is greater")
+  else:
+    print("Second number is greater or equal")
+
+def isLesser(a, b):
+  pass
+  
+
+a = 3
+b = 6
+isGreater(a, b)
+calculateGmean(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c = 1
+d = 2
+isGreater(c, d)
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
