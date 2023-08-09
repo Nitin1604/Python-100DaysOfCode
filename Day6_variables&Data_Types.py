0d84987b711c723eb51fa62ff885689654d96fca
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:23:02 +0530

    Variables and Data Types

---
+++ Day6_variables&Data_Types.py
@@ -0,0 +1,25 @@
+# Day 6 
+a = complex(8, 2)
+print('The value of a is:',a)
+b = True
+print('The value of b is:',b)
+c = "Nitin"
+print('The value of c is:',c)
+d = None
+print('The value of d is:',d)
+a1 = 9
+print('The value of a1 is:',a1)
+print('The value of a+a1 is:',a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
