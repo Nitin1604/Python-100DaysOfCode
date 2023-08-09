52e4c0ba1e337e41564bc61b47abea8da15c8519
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:43:48 +0530

    Import Working

---
+++ Day44_importWorking/Day44_importWorking.py
@@ -0,0 +1,17 @@
+# Day 44
+
+# from math import sqrt, pi
+# from math import pi, sqrt as s
+# import math as math_builtin_python
+
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)  # Output: 3.0
+
+# from nitin import welcome, nitin
+import nitin as ni
+import math
+
+print(dir(math))
+print(math.nan, type(math.nan))
+ni.welcome()
+print(ni.nitin)
---
+++ Day44_importWorking/nitin.py
@@ -0,0 +1,5 @@
+def welcome():
+  print("Hey you are welcome my friend")
+
+
+nitin = "A good boy"
