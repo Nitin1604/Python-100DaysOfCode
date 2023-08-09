1d9ab198142b136dca176e146c308207461517b0
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:25:27 +0530

    Strings

---
+++ Day11_strings.py
@@ -0,0 +1,21 @@
+# Day 11
+name = "Nitin"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+
+# Without using triple single quotes (''' ''') / double quotes (""" """)  | an error will come...
+apple = '''He said, 
+Hi Nitin
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+print(apple)
+print(name[0],name[1], name[2],name[3], name[4])  # N i t i n
+
+# print(name[5]) # Throws an error
+'''
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
+'''
