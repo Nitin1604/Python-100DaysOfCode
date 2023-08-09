0755fd797e65cc79a7e01b63400e3b3b904edd7d
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:39:03 +0530

    For Loop with else

---
+++ Day35_forLoopWithElse.py
@@ -0,0 +1,17 @@
+# Day 35
+
+i = 0
+while i<7:
+  print(i)
+  i = i + 1
+  # if i == 4:
+  #   break
+
+else:
+  print("No variable i iteration")
+
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
