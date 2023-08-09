ca8d931a93395d626eb967d6920867f69f375d3a
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:23:37 +0530

    Calculator Based Exercise

---
+++ Day8_calculator.py
@@ -0,0 +1,8 @@
+# Day 8
+a = 16
+b = 4
+
+print('The addition of ', a, '+', b, 'is:', a+b)
+print('The subtraction of ', a, '-', b, 'is:', a-b)
+print('The multiplication of ', a, '*', b, 'is:', a*b)
+print('The division of ', a, '/', b, 'is:', a/b)
