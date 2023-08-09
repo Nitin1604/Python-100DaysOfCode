b95084c564775bd325669a8664f67bd974deb01a
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:27:09 +0530

    If else loop

---
+++ Day14_if-ElseLoop.py
@@ -0,0 +1,13 @@
+# Day 14
+num = int(input("What's your age:"))
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
