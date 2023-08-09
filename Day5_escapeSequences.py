3107feadf6d835566d9aa5784ca57ef0f6aabb7a
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-09 16:22:40 +0530

    Escape Sequences

---
+++ Day5_escapeSequences.py
@@ -0,0 +1,10 @@
+#<--- Day 5 --->#
+# Escape Sequences: \n
+print('This is the python program \n and including concepts like escape sequences')
+
+# Escape Sequence: \"double quotes\"
+print("I am writting \"double quotes\" in python programming.")
+
+# To insert something in between two words
+print('Separator', 4, 89, sep='-', end='+')
+print('New Line')
