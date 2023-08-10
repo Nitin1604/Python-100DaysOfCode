d52dab073b9a156e94ae585674ae9a973a7aa674
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-10 09:37:46 +0530

    Introduction to Object Oriented Programming

---
+++ Day56_OopsIntro.py
@@ -0,0 +1,25 @@
+# Day 56
+
+def greeting():
+  print("greeting")
+
+greeting()
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+# rajesh.sales
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000 
+# vishal.sales
+
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
+ 
+# RailwayForm   ---> Class [blueprint]
+# nitin --> nitin ki info wala form --> Object [entity]
+# Tushar --> Tushar ki info wala form --> Object [entity]
+# Kunal -- Kunal ki info wala form --> Object [entity]
+# Kunal.changeName("Rajat")
