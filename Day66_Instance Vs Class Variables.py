630e0a1c0a3f3f6ebf840498896a0a0bf4872f92
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-11 13:15:40 +0530

    Instance Vs Class Variables

---
+++ Day66_Instance Vs Class Variables.py
@@ -0,0 +1,24 @@
+# Day 66
+
+class Employee:
+  companyName = "Apple"
+  noOfEmployees = 0
+  def __init__(self, name):
+    self.name = name
+    self.raise_amount = 0.02
+    Employee.noOfEmployees +=1
+  def showDetails(self):
+    print(f"The name of the Employee is:  {self.name} and the raise amount in {self.noOfEmployees} sized {self.companyName} is: {self.raise_amount}")
+    print('')
+
+emp1 = Employee("Nitin")
+Employee.showDetails(emp1)
+emp1.raise_amount = 0.3
+emp1.companyName = "Apple India" 
+emp1.showDetails()
+Employee.companyName = "Google"
+print(Employee.companyName)
+
+emp2 = Employee("Rohan")
+emp2.companyName = "Nestle"
+emp2.showDetails()
