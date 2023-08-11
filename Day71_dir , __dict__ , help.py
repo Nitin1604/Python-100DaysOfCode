029423728c3857e00c0ce757103913f234a155a7
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-11 20:04:28 +0530

    dir , __dict__ and help in Python

---
+++ Day71_dir , __dict__ , help.py
@@ -0,0 +1,13 @@
+# Day 71
+
+class Person:
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+      self.version = 1
+
+    
+p = Person("John Cena", 30)
+print(p.__dict__)
+
+print(help(Person))
