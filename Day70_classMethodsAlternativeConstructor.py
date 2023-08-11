8eb2e70291977e026ab621e78bc7c5b88b392dfe
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-11 17:53:47 +0530

    Class Methods as Alternative Constructor

---
+++ Day70_classMethodsAlternativeConstructor.py
@@ -0,0 +1,32 @@
+# Day 70
+
+class Employee:
+  def __init__(self, name, salary):
+    self.name = name 
+    self.salary = salary
+    
+  @classmethod
+  def fromStr(cls, string):
+    return cls(string.split("-")[0], int(string.split("-")[1]))
+    
+e1 = Employee("Nitin", 12000)
+print(e1.name)
+print(e1.salary)
+
+string = "Rohan-12000"
+e2 = Employee.fromStr(string)
+print(e2.name)
+print(e2.salary)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def from_string(cls, string):
+        name, age = string.split(',')
+        return cls(name, int(age))
+
+person = Person.from_string("Rohan, 30")
+print(person.name, person.age)
