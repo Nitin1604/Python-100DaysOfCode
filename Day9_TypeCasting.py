f4ab3f0f190f8794659c9bf3e391214328a8b4a4
Nitin Sokhal <80270629+Nitin1604@users.noreply.github.com>
2023-08-12 13:27:05 +0530

    Type Casting in Python

---
+++ Day9_TypeCasting.py
@@ -0,0 +1,13 @@
+# Day 9
+a = '16' # 16 is as a string because it is written in single quotes
+b = '25' # 25 is also a string
+
+print(int(a)+int(b))  # String a is converted to int now... This is type casting
+
+# Implicit TypeCasting
+c = 8.9
+d = 8
+
+print(c+d)
+print(str(c) + str(d))
+
